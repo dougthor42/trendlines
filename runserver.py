8d678b85ead5f89418fa62f293c36f9a5aa5de08
Douglas Thor <doug.thor@gmail.com>
2019-01-02 14:25:36 -0800

    Set the TRENDLINES_CONFIG_FILE env var in runserver.py

--- runserver.py
+++ runserver.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+
 from trendlines.app_factory import create_app
 
 PORT = 5001
+
+# Load our configuration. We're using pathlib and `.resolve()` because
+# Flask's working dir is src/trendlines and uses that when running
+# `app.config.from_envvar`, so the path it attemps to load is
+# `/proj_folder/src/trendlines/config/localhost.cfg` if `cfg_file` is not
+# absolute.
+cfg_file = Path('./config/localhost.cfg')
+os.environ['TRENDLINES_CONFIG_FILE'] = str(cfg_file.resolve())
 
 app = create_app()
 app.run(debug=True, port=PORT, use_reloader=True)
