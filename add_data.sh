6aad420a37ded136e73089284957c0132f1db424
Douglas Thor <doug.thor@gmail.com>
2019-01-11 13:49:32 -0800

    Replace 'Content-Type...' with var in add_data.sh. Just makes it cleaner.

--- add_data.sh
+++ add_data.sh
@@ -3,17 +3,18 @@
 PORT=5000
 URL="http://localhost:$PORT"
 API="$URL/api/v1/data"
+C_TYPE="Content-Type: application/json"
 
 for run in {1..10}
 do
-curl -d '{"metric":"foo.bar", "value":'`shuf -i 1-100 -n 1`'}' -H "Content-Type: application/json" -X POST "$API";
+curl -d '{"metric":"foo.bar", "value":'`shuf -i 1-100 -n 1`'}' -H "$C_TYPE" -X POST "$API";
 sleep 0.1;
 done;
 sleep 1;
-curl -d '{"metric":"foo.bar", "value":'`shuf -i 1-100 -n 1`', "time":'`date +%s`'}' -H "Content-Type: application/json" -X POST "$API";
+curl -d '{"metric":"foo.bar", "value":'`shuf -i 1-100 -n 1`', "time":'`date +%s`'}' -H "$C_TYPE" -X POST "$API";
 
 for run in {1..5}
 do
-curl -d '{"metric":"baz", "value":'`shuf -i 1-100 -n 1`'}' -H "Content-Type: application/json" -X POST "$API";
+curl -d '{"metric":"baz", "value":'`shuf -i 1-100 -n 1`'}' -H "$C_TYPE" -X POST "$API";
 sleep 0.3;
 done;
