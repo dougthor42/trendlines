aab709a3f2ce04356aa0e5202522538196876c1f
Douglas Thor <doug.thor@gmail.com>
2019-02-28 07:29:49 -0800

    Explicitly define the PK in migrations: 0001_metric

--- migrations/0001_create_table_metric.py
+++ migrations/0001_create_table_metric.py
@@ -6,7 +6,7 @@ date created: 2019-02-14 22:01:44.111130
 
 def upgrade(migrator):
     with migrator.create_table('metric') as table:
-        table.int('metric_id')
+        table.int('metric_id', primary_key=True)
         table.char('name', max_length=120)
         table.char('units', max_length=24, null=True)
 
