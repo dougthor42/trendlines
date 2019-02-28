ab3f3ce59a1c0dd79595e4e3e304ccd087b97783
Douglas Thor <doug.thor@gmail.com>
2019-02-28 07:30:10 -0800

    Explicitly define the PK in migrations: 0002_datapoint

--- migrations/0002_create_table_datapoint.py
+++ migrations/0002_create_table_datapoint.py
@@ -6,8 +6,14 @@ date created: 2019-02-14 22:01:50.586924
 
 def upgrade(migrator):
     with migrator.create_table('datapoint') as table:
-        table.int('datapoint_id')
-        table.foreign_key('INT', 'metric_id', on_delete=None, on_update=None, references='metric.metric_id')
+        table.int('datapoint_id', primary_key=True)
+        table.foreign_key(
+            'INT',
+            'metric_id',
+            on_delete=None,
+            on_update=None,
+            references='metric.metric_id',
+        )
         table.float('value')
         table.int('timestamp')
 
