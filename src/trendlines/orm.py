9c50975ad5451223f39cb94d997856a842e98c18
Douglas Thor <doug.thor@gmail.com>
2019-02-28 16:34:01 -0800

    Update create_db in orm.py
    + Fix import in orm.py that was lost at some point.
    + Update how we handle peewee.OperationalError:
    
      Previously we were checking things with Path.resolve(), but that
      won't catch everything because we can't use `resolve(strict=True)`
      (need to support py35 which doesn't have that).
    
      So instead we try connecting to the database, which creates the file
      if needed, and if there's an error we see if the file exists or not.
    + Use the full path when doing copies in create_db
    + Check if the database file exists at the start of orm.create_db
    + Add comments explaining things such as when files are modified.
    + Only create a backup if the database file already exists.
    + Add a boatload of explainatory comments
    + Only revert to the backup if an original file existed.
    + Delete the backup file if no migrations were applied.
    + Handle the case where DatabaseManager tries to open up `/migrations`
      when running in docker. The correct path there is
      `/trendlines/migrations`.

--- src/trendlines/orm.py
+++ src/trendlines/orm.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import shutil
+from datetime import datetime
 from pathlib import Path
 
 from peewee import SqliteDatabase
@@ -9,8 +11,11 @@ from peewee import TimestampField
 from peewee import ForeignKeyField
 from peewee import CharField
 from peewee import OperationalError
+from peewee_moves import DatabaseManager
 
 from trendlines import logger
+from trendlines import utils
+from trendlines.__about__ import __project_url__
 
 
 DB_OPTS = {
@@ -95,33 +100,102 @@ def create_db(name):
     """
     Create the database and the tables.
 
-    Does nothing if the tables already exist. Well, techinically it just
-    connects and disconnects - ``create_tables`` is a noop.
+    Applies any missing migrations. Does nothing if all migrations
+    have been applied.
 
     Parameters
     ----------
     name : str
-        The name of the database, as given by ``app.config['DATABASE']``.
+        The name/path of the database, as given by ``app.config['DATABASE']``.
     """
-    logger.debug("Creating database: '%s'." % name)
-    db.init(name, pragmas=DB_OPTS)
+    #  import pdb; pdb.set_trace()
+    # Convert to a Path object because I like working with those better.
+    full_path = Path(name).resolve()
+
+    file_exists = full_path.exists()
+    if file_exists:
+        logger.debug("Connecting to existing database: '%s'." % full_path)
+    else:
+        logger.debug("Creating new database: '%s'" % full_path)
+
+    db.init(str(full_path), pragmas=DB_OPTS)
 
     try:
+        # This will create the file if it doesn't exist.
         db.connect()
     except OperationalError:
-        # .resolve() will fail for missing paths, and the `strit=False` arg
-        # wasn't added until 3.6. Since dev environment is 3.5 I need
-        # this try..except block.
-        try:        # pragma: no cover
-            full_path = Path(name).resolve()
-        except FileNotFoundError:
-            full_path = name
-        logger.error("Unable to open database file '%s'" % full_path)
+        # Try to figure out why OperationalError happened.
+        if file_exists:
+            msg = ("Database file %s exists, but we're unable to connect."
+                   " Perhaps the permissions are incorrect?")
+            logger.error(msg % full_path)
+        else:
+            msg = ("Unable to create %s. Perhaps the parent folder is missing"
+                   " or permissions are incorrect?")
+            logger.error(msg % full_path)
+        logger.error("Unable to create/open database file '%s'" % full_path)
         raise
 
-    tables = [
-        Metric,
-        DataPoint,
-    ]
-    db.create_tables(tables)
+    # Either way, we want to run migrations. However, we only need to make a
+    # backup if the file already exists.
+    if file_exists:
+        # Create a backup before doing anything.
+        backup_file = utils.backup_file(full_path)
+        logger.debug("Created database backup file: {}".format(backup_file))
+
+    # This will edit the database file, creating the `migration_history`
+    # table if needed. Hence why we do it *after* the backup.
+    try:
+        manager = DatabaseManager(db)
+    except PermissionError:
+        # When runnng in the docker container, this will attempt to create
+        # a `/migrations` directory. It should be `/trendlines/migrations`.
+        # If this still fails, let the error propogate but make sure to
+        # close the db connection
+        try:
+            msg = "Failed to open default migration directory, trying '%s'"
+            alt_dir = "/trendlines/migrations"
+            logger.debug(msg % alt_dir)
+            manager = DatabaseManager(db, directory=alt_dir)
+            logger.debug("Success")
+        except PermissionError:
+            raise
+        finally:
+            db.close()
+
+    # Check the status. Creating a new file means we'll need migrations.
+    # However, we don't need to check for that because it's guaranteed
+    # that a new file will have len(manager.diff) > 0
+    needs_migrations = len(manager.diff) > 0
+
+    if needs_migrations:
+        logger.info("Missing migrations: {}".format(manager.diff))
+
+        # Apply the migrations
+        success = manager.upgrade()
+        if success:
+            logger.info("Successfully applied database migrations.")
+        elif file_exists:
+            # revert our changes by restoring the backup
+            msg = ("Failed to apply database migrations. Reverting to backup"
+                   " file. Please submit an issue at {} with details.")
+            logger.critical(msg.format(__project_url__))
+            shutil.copy(str(backup_file), str(full_path))
+        else:
+            # It's a new file, so no backup was made.
+            msg = ("Failed to apply database migrations to the new file."
+                   " Please see the logs for more info.")
+            logger.critical(msg)
+    else:
+        logger.info("Database is up to date. No migrations to apply.")
+        # Since we didn't make any changes, we can remove the backup file.
+        # Is it possible to ever have backup_file not exist if we didn't
+        # apply migrations?
+        # No, because not applying migrations implies that the file already
+        # existed, and if the file already existed then a backup was made.
+        # Thus we don't need to check for FileNotFoundError.
+        backup_file.unlink()
+        logger.debug("Removed superfluous backup file: %s" % backup_file)
+
+    # Make sure to close the database if things went well.
     db.close()
