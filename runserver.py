82bba611a034f348c352fa0ed059e54fb7a0cd99
Douglas Thor <doug.thor@gmail.com>
2019-05-01 10:34:47 -0700

    Switch to using `click` in runserver.py

--- runserver.py
+++ runserver.py
@@ -2,17 +2,31 @@
 import os
 from pathlib import Path
 
+import click
+
+import trendlines.orm as orm
 from trendlines.app_factory import create_app
 
 PORT = 5000
 
-# Load our configuration. We're using pathlib and `.resolve()` because
-# Flask's working dir is src/trendlines and uses that when running
-# `app.config.from_envvar`, so the path it attemps to load is
-# `/proj_folder/src/trendlines/config/localhost.cfg` if `cfg_file` is not
-# absolute.
-cfg_file = Path('./config/localhost.cfg')
-os.environ['TRENDLINES_CONFIG_FILE'] = str(cfg_file.resolve())
 
-app = create_app()
-app.run(debug=True, port=PORT, use_reloader=True)
+def runserver():
+    # Load our configuration. We're using pathlib and `.resolve()` because
+    # Flask's working dir is src/trendlines and uses that when running
+    # `app.config.from_envvar`, so the path it attemps to load is
+    # `/proj_folder/src/trendlines/config/localhost.cfg` if `cfg_file` is not
+    # absolute.
+    cfg_file = Path('./config/localhost.cfg')
+    os.environ['TRENDLINES_CONFIG_FILE'] = str(cfg_file.resolve())
+
+    app = create_app()
+    app.run(debug=True, port=PORT, use_reloader=True)
+
+
+@click.command()
+def main(db_from_orm):
+    runserver()
+
+
+if __name__ == "__main__":
+    main()
