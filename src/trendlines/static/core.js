04b201a10674beef542e7f7db39bc902d44ae844
Douglas Thor <doug.thor@gmail.com>
2019-07-10 18:30:29 -0700

    Add the URL_PREFIX value to populateTree in core.js. Fixes #162.

--- src/trendlines/static/core.js
+++ src/trendlines/static/core.js
@@ -1,7 +1,7 @@
 /**
  * Populate the JSTree tree.
  */
-function populateTree(data, metricId) {
+function populateTree(data, metricId, urlPrefix) {
   var tree = $('#jstree-div');
 
   // Create an instance when the DOM is ready.
@@ -35,7 +35,7 @@ function populateTree(data, metricId) {
 
   // Update the plot or toggle the node open/closed.
   tree.on('select_node.jstree', function(e, data) {
-    treeChanged(e, data);
+    treeChanged(e, data, urlPrefix);
   });
 };
 
@@ -44,10 +44,13 @@ function populateTree(data, metricId) {
  * Update the plot if data exists, otherwise just open the tree.
  * This is called when the `select_node` event is seen.
  */
-function treeChanged(e, data) {
+function treeChanged(e, data, urlPrefix) {
   // If `metric_id` is defined, then we can query data
   if (data.node.original.metric_id !== null) {
-    var expected = "/api/v1/data/" + data.node.original.metric_id;
+
+    // Stupid f-ing javascript... urlPrefix can be `null`, which when concatenated
+    // with a string, gets cast to the string 'null'. So we get "null/api/..."
+    var expected = (urlPrefix || "") + "/api/v1/data/" + data.node.original.metric_id;
     // grab the plot data from the api
     $.getJSON(expected)
       .done(function(jsonData) {
